a39fb500cb6bda2c3c4f30ae8e44acdf798345f7
Scott <james.scott@mclaren.com>
2017-12-30 16:59:35 +0000

    amended to output JSON file to disk

--- DarwinHPS.java
+++ DarwinHPS.java
@@ -36,6 +36,11 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import org.apache.commons.io.IOUtils;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+
 
 public class DarwinHPS {
 
@@ -55,12 +60,12 @@ public class DarwinHPS {
 		httpPost.addHeader(new BasicScheme().authenticate(creds, httpPost, null));
 		
 		Map<String, String> comment = new HashMap<String, String>();
-		comment.put("from_loc","BTN");
-		comment.put("to_loc","VIC");
+		comment.put("from_loc","WOK");
+		comment.put("to_loc","WAT");
 		comment.put("from_time","0800");
 		comment.put("to_time","1000");
-		comment.put("from_date","2017-12-28");
-		comment.put("to_date","2017-12-29");
+		comment.put("from_date","2016-12-31");
+		comment.put("to_date","2017-12-31");
 		comment.put("days","WEEKDAY");
 		//comment.put("rid", "201607294212242");
 		String json = new GsonBuilder().create().toJson(comment, Map.class);
@@ -79,25 +84,36 @@ public class DarwinHPS {
 			//parse the JSON object
 			// HttpEntity entity = response2.getEntity().getContent();
 			// Object content = EntityUtils.toString(entity);
-			String json_string = EntityUtils.toString(response2.getEntity());
-			System.out.println(json_string);
 			
-			JsonObject jobj = new Gson().fromJson(json_string, JsonObject.class);
+			HttpEntity entity = response2.getEntity();
+			//String json_string = EntityUtils.toString(entity);
+			//System.out.println(json_string);
+			
+			//JsonObject jobj = new Gson().fromJson(json_string, JsonObject.class);
 
-			JsonArray servicesArray = jobj.get("Services").getAsJsonArray();
+			//JsonArray servicesArray = jobj.get("Services").getAsJsonArray();
 			
 			
-			JsonObject servicesObj = jobj.get("Services").getAsJsonObject();
-			JsonArray serviceMetricsArr = servicesObj.get("serviceAttributesMetrics").getAsJsonArray();
+			//JsonObject servicesObj = jobj.get("Services").getAsJsonObject();
+			//JsonArray serviceMetricsArr = servicesObj.get("serviceAttributesMetrics").getAsJsonArray();
 			
 			//System.out.println(serviceMetricsObj.get("origin_location").getAsString());
 			//System.out.println(value);	
 
-			
-			//EntityUtils.consume(entity2);
+			//Write to JSON file
+
+			BufferedInputStream bis = new BufferedInputStream(entity.getContent());
+			String filePath = "Darwin.JSON";
+			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File(filePath)));
+			int inByte;
+			while((inByte = bis.read()) != -1) bos.write(inByte);
+			bis.close();
+			bos.close();
+
+			EntityUtils.consume(entity);
 		} finally {
 			httpPost.releaseConnection();
 			}
 
-	} 
- }
+	} 	
+	}
